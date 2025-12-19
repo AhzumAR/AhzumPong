e6e07b2d876c3017051c3e847487f7aa0cad9e83
AhzumAR <Ahzumar@gmail.com>
2025-12-18 18:44:06 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,7 +55,13 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("TenseMusic.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -5; //turn down volume
+
+document.addEventListener("pointerdown", () => {
+  Tone.start().then(() => {
+    ambientSound.player.start();
+  });
+}, { once: true });
 
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
